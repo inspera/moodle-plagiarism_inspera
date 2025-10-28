0f6bd1f5a0661e1b8af2e97762551b74e3499b6d
Carlo Giombolini <carlo.giombolini@inspera.no>
2025-10-28 14:35:41 +0100

    IB-13955, fixed requires moodle version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'plagiarism_originality'; // Full name of the plugin in frankenstyle.
 $plugin->version   = 2025100104;               // Plugin version (YYYYMMDDXX).
-$plugin->requires  = 2024101400;               // Minimum Moodle version (e.g., 4.5 stable).
+$plugin->requires  = 2024100700;               // Minimum Moodle version (e.g., 4.5 stable).
 $plugin->maturity  = MATURITY_ALPHA;           // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE.
 $plugin->release   = '0.1.0';                  // Human-readable version.
