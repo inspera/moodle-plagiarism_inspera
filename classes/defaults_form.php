05ce795f5c08a5a878127c0493fb19a671ba079f
Carlo Giombolini <carlo.giombolini@inspera.no>
2026-03-23 14:22:14 +0100

    IB-18122 Fix Advanced visibility cascade and locked UI for dependent settings

--- classes/defaults_form.php
+++ classes/defaults_form.php
@@ -293,7 +293,14 @@ class plagiarism_inspera_defaults_form extends moodleform {
             }
 
             $items = [];
+            // Exclude only the translation languages list, as Admins cannot pre-select its values.
+            $dependent_settings = [
+                'originality_translation_languages',
+            ];
             foreach (plagiarism_plugin_inspera::config_options() as $setting) {
+                if (in_array($setting, $dependent_settings, true)) {
+                    continue; // Skip adding this to the Admin multi-select lists.
+                }
                 $items[$setting] = get_string($setting, 'plagiarism_inspera');
             }
 
