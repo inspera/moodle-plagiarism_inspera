31128db33c6469037831d0ba3ef83620cece3219
Carlo Giombolini <carlo.giombolini@inspera.no>
2026-04-09 11:13:58 +0200

    IB-18664 Style: Remove redundant MOODLE_INTERNAL check to satisfy Moodle Codechecker

--- tests/workshop_service_test.php
+++ tests/workshop_service_test.php
@@ -25,8 +25,6 @@
 
 namespace plagiarism_inspera;
 
-defined('MOODLE_INTERNAL') || die();
-
 use advanced_testcase;
 use plagiarism_inspera\services\workshop_service;
 use plagiarism_inspera\services\queue_service;
